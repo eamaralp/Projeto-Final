66b1438588f7f67e13fceffcea7f96ae563bb4cb
eamaralp <eduardoamaralp@gmail.com>
2015-10-26 23:28:19 -0200

    beta teste
    versao beta apresentação final

--- projeto_apresentacao.ino/projeto_apresentacao.ino.ino
+++ projeto_apresentacao.ino/projeto_apresentacao.ino.ino
@@ -1,8 +1,11 @@
 #include "EmonLib.h"                   // Include Emon Library
 #include <SPI.h>
+#include "String.h"
 #include <LCD5110_Graph_SPI.h>  
 
-char TIrms[40];
+char irmsAux[40];
+char aux01[20], aux02[20], aux03[20];
+String strg01, strg02, strg03, strgTotal;
 LCD5110 myGLCD(5,6,3);
 
 EnergyMonitor emon1, emon2, emon3;                   // Create an instance
@@ -26,43 +29,69 @@ void setup()
 
 void loop()
 { 
-  double Irms01 = emon1.calcIrms(1480);  // Calcula Corrente
-  double Irms02 = emon2.calcIrms(1480);  // Calcula Corrente
-  double Irms03 = emon3.calcIrms(1480);  // Calcula Corrente
-  double Pot01 = Irms01*111.1; // Calcula Potencia
-  double Pot02 = Irms02*111.1; // Calcula Potencia
-  double Pot03 = Irms03*111.1; // Calcula Potencia
-  
-  if(Pot01 > 1 or Pot02 > 1 or Pot03 > 1)
+  // Calcula Corrente
+  double irms01 = emon1.calcIrms(1480);  
+  double irms02 = emon2.calcIrms(1480);  
+  double irms03 = emon3.calcIrms(1480); 
+  // Calcula Potencia
+  double pot01 = irms01*111.1; 
+  double pot02 = irms02*111.1; 
+  double pot03 = irms03*111.1; 
+
+  // Valores abaixo de 1 watt não sao computados, o calculo realizado é o de kw/w
+  // os valores são acumulados em uma única variável para apresentar o kw/h total
+  if(pot01 > 1 or pot02 > 1 or pot03 > 1)
   {
     ltmillis = tmillis;
     tmillis = millis();
     timems = tmillis - ltmillis;
-    kwhTotal = kwhTotal + (((Irms01*111.1)/1000.0) * 1.0/3600.0 * (timems/1000.0)); 
-    kwhTotal = kwhTotal + (((Irms02*111.1)/1000.0) * 1.0/3600.0 * (timems/1000.0)); 
-    kwhTotal = kwhTotal + (((Irms03*111.1)/1000.0) * 1.0/3600.0 * (timems/1000.0)); 
+    kwhTotal = kwhTotal + (((irms01*111.1)/1000.0) * 1.0/3600.0 * (timems/1000.0)); 
+    kwhTotal = kwhTotal + (((irms02*111.1)/1000.0) * 1.0/3600.0 * (timems/1000.0)); 
+    kwhTotal = kwhTotal + (((irms03*111.1)/1000.0) * 1.0/3600.0 * (timems/1000.0)); 
   }
-  else{
-    Pot01 = 0;
+  // Para a apresentação visual valores menores que 2 watts não sao apresentados no display
+  if(pot01 < 2)
+  {
+    pot01 = 0;  
   }
+  if(pot02 < 2)
+  {
+    pot02 = 0;  
+  }
+  if(pot03 < 2)
+  {
+    pot03 = 0;  
+  }
+  
+  myGLCD.clrScr(); // limpando o diplay lcd
 
-
-  dtostrf(Irms01, 3, 3, TIrms);
-  myGLCD.clrScr();
-  myGLCD.print("S1___", LEFT, 0);
-  myGLCD.print("S2___", LEFT, 8);
-  myGLCD.print("S3___", LEFT, 17);
-  myGLCD.print("TOTAL:", LEFT, 25);
-  dtostrf(kwhTotal, 4, 5, TIrms);
-  myGLCD.print(TIrms, CENTER, 38);          
-  dtostrf(Pot01, 4, 0, TIrms);
-  myGLCD.clrLine(0, 34, 80, 45);
-  myGLCD.print(TIrms, CENTER, 0);          
+  // convertendo a corrente para string e preparando para apresentar no display
+  myGLCD.print("Sensor1.....", LEFT, 0);;
+  dtostrf(pot01, 4, 0, irmsAux);
+  strg01 = irmsAux;
+  strg01 += "W";
+  myGLCD.print(strg01, RIGHT, 0);
+  
+  myGLCD.print("Sensor2.....", LEFT, 10);
+  dtostrf(pot02, 4, 0, irmsAux);
+  strg02 = irmsAux;
+  strg02 += "W";
+  myGLCD.print(strg02, RIGHT, 10);
+  
+  myGLCD.print("Sensor3.....", LEFT, 21);
+  dtostrf(pot03, 4, 0, irmsAux);
+  strg03 = irmsAux;
+  strg03 += "W";
+  myGLCD.print(strg03, RIGHT, 21);
+  
+  myGLCD.print("T:", LEFT, 38);
+  dtostrf(kwhTotal, 4, 1, irmsAux);
+  strgTotal = irmsAux;
+  strgTotal += " Kw/h";
+  myGLCD.print(strgTotal, RIGHT, 38);          
+          
   myGLCD.update();
-  Serial.print("Irms:");
-  Serial.println(TIrms);           // Irms
   
   delay(550);
-//  myGLCD.update();
 
 }
